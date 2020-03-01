31c2b76e2b0686bb4b1b17f1f28eb09cce3ec3ed
Herman Li <herman_hmli@yahoo.com>
2020-02-29 21:58:05 -1000

    Add temp login path

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json())
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/login', (req, res) => {
+    cred = req.body;
+
+    if(cred.username === 'admin' && cred.password === 'admin') {
+        res.json({
+            loggedIn: true,
+            apiKey: "<placeholder>"
+        });
+    } else {
+        res.json({
+            loggedIn: false
+        });
+    }
+    
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
